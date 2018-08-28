5ba33476e174bd7c0c642c349cbe5071d39a6404
Anwesh43 <anweshthecool0@gmail.com>
2018-08-29 01:41:55 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedhoritriincview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedhoritriincview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedhoritriincview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.horitriincview.HoriTriIncView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        HoriTriIncView.create(this)
     }
 }
