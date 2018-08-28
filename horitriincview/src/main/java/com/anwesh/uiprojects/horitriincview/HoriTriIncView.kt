b2cc53964538803ced9e239602fb01685673c987
Anwesh43 <anweshthecool0@gmail.com>
2018-08-29 00:50:57 +0530

    importing libraries and defining nodes

--- horitriincview/src/main/java/com/anwesh/uiprojects/horitriincview/HoriTriIncView.kt
+++ horitriincview/src/main/java/com/anwesh/uiprojects/horitriincview/HoriTriIncView.kt
@@ -3,3 +3,13 @@ package com.anwesh.uiprojects.horitriincview
 /**
  * Created by anweshmishra on 29/08/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Path
+
+val nodes : Int = 5
+
